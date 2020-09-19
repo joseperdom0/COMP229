2663715bbdbbe105e59770f1e452960273be7d96
joseperdom0 <aguilar.roy@gmail.com>
2020-09-18 23:50:39 -0500

    Inital commit

---
+++ Week01Ex.js
@@ -0,0 +1,16 @@
+//Notes
+
+/*Suppose you have a file 
+named 
+lib.js
+ that contains the following code:
+export function halfOf(x) {
+return x / 2;
+}
+
+So, in your 
+main.js
+ file, you can use the following code:
+import halfOf from 'lib';
+console.log(halfOf(84))
+*/
